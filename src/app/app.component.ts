9d6eda44495157ad207333cecaf44b89a7906465
birdman <abc@nowhere.com>
2019-06-23 18:38:38 +0530

    first commit angular 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+
+export function log(className){
+  console.log(className);
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
+
+@log
 export class AppComponent {
   title = 'ng7App';
+
+  constructor(){
+    console.log("inside constructor");
+  }
+
+  myCustomFunc(){
+    console.log("Inside my custom Func");
+  }
 }
+
+
+
+
+
